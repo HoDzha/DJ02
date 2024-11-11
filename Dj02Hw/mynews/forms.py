5e1b70153aacfedda722d82b04f7919c36cd409b
HoDzha <hodzha@gmail.com>
2024-11-11 22:50:43 +0500

    До изменений

--- Dj02Hw/mynews/forms.py
+++ Dj02Hw/mynews/forms.py
@@ -1,3 +1,4 @@
+# forms.py
 from .models import My_News_post
 from django.forms import ModelForm, TextInput, Textarea, DateTimeInput
 
@@ -6,9 +7,11 @@ class My_News_postForm(ModelForm):
 		model = My_News_post
 		fields = ['user_name', 'title', 'short_description', 'text', 'pub_date']
 		widgets = {
-			'user_name': Label(attrs={'class': 'form-control', 'placeholder': 'Автор новости'}),
-			'title': TextInput(attrs={'class': 'form-control', 'placeholder': 'Заголовок новости'}),
-			'short_description': TextInput(attrs={'class': 'form-control', 'placeholder': 'Краткое описание новости'}),
-			'text': Textarea(attrs={'class': 'form-control', 'placeholder': 'Содержание новости'}),
-			'pub_date': DateTimeInput(attrs={'class': 'form-control', 'placeholder': 'Дата публикации'})
+			'user_name': TextInput(attrs={'class': 'form-control col-6', 'placeholder': 'Автор новости'}),
+			'title': TextInput(attrs={'class': 'form-control col-6', 'placeholder': 'Заголовок новости'}),
+			'short_description': TextInput(
+				attrs={'class': 'form-control col-12', 'placeholder': 'Краткое описание новости'}),
+			'text': Textarea(attrs={'class': 'form-control col-12', 'placeholder': 'Содержание новости', 'rows': 4}),
+			'pub_date': DateTimeInput(
+				attrs={'class': 'form-control col-4', 'placeholder': 'Дата публикации', 'type': 'datetime-local'}),
 		}
